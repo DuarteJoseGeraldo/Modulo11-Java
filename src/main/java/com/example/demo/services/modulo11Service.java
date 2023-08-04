300f6ddf0e02c156a1e9fa43d35a09844cc55c41
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-08-04 17:20:05 -0300

    first commit

---
+++ src/main/java/com/example/demo/services/modulo11Service.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class modulo11Service {
+    public boolean test(int number){
+        return number > 10;
+    }
+
+}
