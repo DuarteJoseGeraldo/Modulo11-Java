8ff4654ab4102da9f53f709eb1a0b6afdd20b5d3
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-08-08 17:25:34 -0300

    Update personData.java

--- src/main/java/com/example/demo/models/personData.java
+++ src/main/java/com/example/demo/models/personData.java
@@ -1,9 +1,11 @@
 package com.example.demo.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder @Data
+@Builder @Data @NoArgsConstructor @AllArgsConstructor
 public class personData {
     private String name;
     private String cpf;
